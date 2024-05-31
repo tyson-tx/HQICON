03f1a1a9006df201fdd5cc02278fa8008a64175b
tanxu <tysontx@163.com>
2024-05-31 20:53:41 +0800

    Time out modification

--- app/api/icons/route.tsx
+++ app/api/icons/route.tsx
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-export const maxDuration = 120;
+export const maxDuration = 60;
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
